128384a14a61749346848952c6cf661e3ec348c4
Ankit <maheshwari.ankit@gmail.com>
2025-11-24 18:25:42 +0530

    chapter 3

--- chapter-03/final/practice-01.py
+++ chapter-03/final/practice-01.py
@@ -1,22 +1,20 @@
 import streamlit as st
 
+st.title("Forms & Widgets")
+
 # practice 1 | widgets
 # -----------------------------------
-st.title("Widgets")
+st.header("Widgets")
 name = st.text_input("Your name")
 age = st.number_input("Your age", min_value=0, max_value=120, value=25)
 if st.button("Submit"):
     st.success(f"Hello {name}, you are {age} years old!")
 
 
-
-
-
-
 # practice 2 | checkBox, selectbox| widgets
 # ----------------------------------------
 
-st.title("Selection & Slider ")
+st.header("Selection & Slider ")
 # Selection
 color = st.selectbox("Favorite color", ["Red", "Green", "Blue"])
 agree = st.checkbox("I agree to terms")
@@ -28,11 +26,10 @@ st.write(f"Agreed: {agree}")
 st.write(f"Rating: {rating}/10")
 
 
-
 # Basic form structure
 # Registration form
 # -----------------------------------
-st.title("Registration Form")
+st.header("Registration Form")
 with st.form("registration"):
     name = st.text_input("Full Name")
     email = st.text_input("Email")
